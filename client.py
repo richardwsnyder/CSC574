d290f6d0b8056cc023a9a510aa7075ce69d7ac3f
Richard <richard.snyder@knights.ucf.edu>
2020-05-01 14:02:06 -0400

    get permissions added

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import pickle
 import socket
 import threading
 import time
+from random import random
 
 from Crypto.PublicKey import RSA
 from Crypto.Random import get_random_bytes
@@ -15,26 +16,20 @@ PORT = 65432
 
 format = '%(asctime)s: %(message)s'
 logging.basicConfig(format=format, level=logging.INFO, datefmt='%H:%M:%S')
+permissions = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'alpha', 'beta', 'gamma', 'delta']
 
-def client_thread(msg, session_key, nonce, tag, thread_num):
-    # change the RSA key size per implementation
-    key = RSA.generate(1024)
-
-    # write the private key
-    private_key = key.export_key()
-    file_out = open('client-private-{}.pem'.format(thread_num), 'wb')
-    file_out.write(private_key)
-    file_out.close()
-
-    # write the public key
-    public_key = key.publickey().export_key()
-    file_out = open('client-receiver-{}.pem'.format(thread_num), 'wb')
-    file_out.write(public_key)
-    file_out.close()
+def client_thread(msg, session_key, nonce, tag, pub_key):
     # logging.info('I am thread {}'.format(thread_num))
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as c:
         c.connect((HOST, PORT))
-        arr = (msg, session_key, nonce, tag, public_key, 'common')
+        perm_list = {}
+        for perm in permissions:
+            ran = random()
+            if ran < 0.5:
+                perm_list[perm] = False
+            else:
+                perm_list[perm] = True
+        arr = (msg, session_key, nonce, tag, pub_key, 'common', perm_list)
         data_string = pickle.dumps(arr)
         c.send(data_string)
         data = c.recv(1024)
@@ -73,12 +68,23 @@ cipher_aes = AES.new(session_key, AES.MODE_EAX)
 ciphertext, tag = cipher_aes.encrypt_and_digest('bootstrap'.encode('utf-8'))
 bootstrap(ciphertext, session_key, cipher_aes.nonce, tag)
 
-bootstrap(ciphertext, session_key, cipher_aes.nonce, tag)
-
-time.sleep(5)
-
-
 for i in range(10):
+
+    # change the RSA key size per implementation
+    key = RSA.generate(1024)
+
+    # write the private key
+    private_key = key.export_key()
+    file_out = open('client-private-{}.pem'.format(i), 'wb')
+    file_out.write(private_key)
+    file_out.close()
+
+    # write the public key
+    public_key = key.publickey().export_key()
+    file_out = open('client-receiver-{}.pem'.format(i), 'wb')
+    file_out.write(public_key)
+    file_out.close()
+
     # msg that will be sent to the server
     msg = 'adduser'.encode('utf-8')
 
@@ -91,5 +97,22 @@ for i in range(10):
     ciphertext, tag = cipher_aes.encrypt_and_digest(msg)
 
     # send message, session key, nonce, tag, and thread #
-    t = threading.Thread(target=client_thread, args=(ciphertext, session_key, cipher_aes.nonce, tag, i))
+    t = threading.Thread(target=client_thread, args=(ciphertext, session_key, cipher_aes.nonce, tag, public_key))
+    t.start()
+
+    t.join()
+
+    # msg that will be sent to the server
+    msg = 'getpermissions'.encode('utf-8')
+
+    # get server public key and create session key
+    recipient_key = RSA.importKey(open('server-receiver.pem').read())
+    session_key = get_random_bytes(16)
+
+    # encrypt the message
+    cipher_aes = AES.new(session_key, AES.MODE_EAX)
+    ciphertext, tag = cipher_aes.encrypt_and_digest(msg)
+
+    # send message, session key, nonce, tag, and thread #
+    t = threading.Thread(target=client_thread, args=(ciphertext, session_key, cipher_aes.nonce, tag, public_key))
     t.start()
--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import signal
 import socket
 from sys import getsizeof
 import threading
+import time
 
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
@@ -30,22 +31,35 @@ format = '%(asctime)s: %(message)s'
 logging.basicConfig(format=format, level=logging.INFO, datefmt='%H:%M:%S')
 
 designations = {}
+default_master_permissions = {'one': True, 'two': True, 'three': True, 'four': True, 'five': True, 'six': True, 'seven': True, 'eight': True, 'nine': True, 
+                              'alpha': True, 'beta': True, 'gamma': True, 'delta': True}
+default_common_permissions = {'one': False, 'two': False, 'three': False, 'four': False, 'five': False, 'six': False, 'seven': False, 'eight': False, 'nine': False, 
+                              'alpha': False, 'beta': False, 'gamma': False, 'delta': False}
+user_permissions = {}
 
 first_user = True
 
 def keyboardInterruptHandler(signal, frame):
     print('KeyboardInterrupt (ID: {}) has been caught. Cleaning up...'.format(signal))
     print('len:', len(designations))
-    print('sizeof:', getsizeof(designations))
+    print('sizeof designations:', getsizeof(designations))
+    print('sizeof user_permissions:', getsizeof(user_permissions))
     exit(0)
 
 signal.signal(signal.SIGINT, keyboardInterruptHandler)
 
-def add_user(pub_key, designation):
+def add_user(pub_key, designation, perm_list):
     if pub_key in designations:
         return 'Already in node'
     designations[pub_key] = designation
-    return 'New user added'
+    if designation == 'common':
+        user_permission = default_common_permissions.copy()
+        for perm in perm_list:
+            user_permission[perm] = perm_list[perm]
+        user_permissions[pub_key] = user_permission
+    else:
+        user_permissions[pub_key] = default_master_permissions
+    return 'New user added with pub_key ' + str(pub_key)
 
 def bootstrap(pub_key):
     global first_user
@@ -58,8 +72,17 @@ def bootstrap(pub_key):
     designations[pub_key] = designation
     return 'Successfully bootstrapped'
 
+def get_permissions(pub_key):
+    permissions = {}
+    try:
+        permissions = user_permissions[pub_key]
+        return str(permissions)
+    except KeyError:
+        return 'public key does not exist in permissions matrix'
+
+
 def server_thread(conn, addr):
-    logging.info('Connection with addr %s: starting', addr)
+    # logging.info('Connection with addr %s: starting', addr)
     with conn:
         # receive msg, session key, nonce, and tag
         data = conn.recv(2048)
@@ -67,6 +90,7 @@ def server_thread(conn, addr):
         if not data:
             ret = 'Did not properly send data'.encode()
             conn.send(ret)
+        begin = time.time()
         data_arr = pickle.loads(data)
         msg = data_arr[0]
         session_key = data_arr[1]
@@ -82,13 +106,17 @@ def server_thread(conn, addr):
 
         if decoded_encrypted_data == 'bootstrap':
             ret = bootstrap(pub_key).encode()
-            print(ret)
 
         elif decoded_encrypted_data == 'adduser':
             designation = data_arr[5]
-            ret = add_user(pub_key, designation).encode()
-            print(ret)
-        # send back unencrypted version of msg
+            perm_list = data_arr[6]
+            ret = add_user(pub_key, designation, perm_list).encode()
+
+        elif decoded_encrypted_data == 'getpermissions':
+            ret = get_permissions(pub_key).encode()
+        end = time.time()
+        # print('Time it took to do {}: {}'.format(decoded_encrypted_data, end - begin))
+        # send back return message
         conn.send(ret)
 
 logging.info('Main: about to start server')
